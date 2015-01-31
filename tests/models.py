0845db1063c869055a94933894e33ac2355887c2
Riccardo Vianello <riccardo.vianello@gmail.com>
2015-01-31 18:06:23 +0100

    test a (slightly) more extended database

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,5 @@
+#from __future__ import unicode_literals
+
 from django_rdkit.db import models
 
 class TestModel(models.Model):
--- tests/tests.py
+++ tests/tests.py
@@ -1,58 +1,40 @@
+#from __future__ import unicode_literals
+
 from django.core.management import call_command
 from django.test import TestCase
 
 from .models import TestModel
-
-class MigrationsTests(TestCase):
-    def test_makemigrations(self):
-        call_command('makemigrations', dry_run=True)
-
+from .molecules import SMILES_SAMPLE
 
 class MoleculeFieldTest(TestCase):
     
     def setUp(self):
-
-        molecules = [
-            'c1ccccc1',
-            'c1ccccc1C',
-            'c1cc(C)ccc1C',
-            'n1ccccc1',
-            'n1ccccc1C',
-            'n1cc(C)ccc1C',
-            'n1ccccc1',
-            'C1CCCCC1',
-        ]
-
-        for molecule in molecules:
-            TestModel.objects.create(molecule=molecule)
+        for smiles in SMILES_SAMPLE:
+            TestModel.objects.create(molecule=smiles)
 
     def test_exact_lookup(self):
 
-        count = TestModel.objects.filter(molecule='C1=CC=CC=C1').count()
-        self.assertEqual(count, 1)
+        objs = TestModel.objects.filter(molecule='COC(c1ccccc1)c1ccccc1')
+        self.assertEqual(objs.count(), 1)
 
-        count = TestModel.objects.filter(molecule='C1C=CCCC1').count()
-        self.assertEqual(count, 0)
-
-        count = TestModel.objects.filter(molecule='C1=CN=CC=C1').count()
-        self.assertEqual(count, 2)
+        objs = TestModel.objects.filter(molecule='Nc1ccc(Cl)nc1')
+        self.assertEqual(objs.count(), 1)
 
     def test_hassubstruct_lookup(self):
 
         objs = TestModel.objects.filter(molecule__hassubstruct='C1=C(C)C=CC=C1')
-        self.assertEqual(objs.count(), 2)
+        self.assertEqual(objs.count(), 61)
 
         objs = TestModel.objects.filter(molecule__hassubstruct='C1=CN=CC=C1')
-        self.assertEqual(objs.count(), 4)
+        self.assertEqual(objs.count(), 7)
 
     def test_issubstruct_lookup(self):
 
-        objs = TestModel.objects.filter(molecule__issubstruct='C1=C(C)C=CC=C1')
+        objs = TestModel.objects.filter(molecule__issubstruct='CCN1c2ccccc2Sc2ccccc21')
         self.assertEqual(objs.count(), 2)
 
-        objs = TestModel.objects.filter(molecule__issubstruct='C1=C(C)N=CC=C1')
-        self.assertEqual(objs.count(), 3)
-
+        objs = TestModel.objects.filter(molecule__issubstruct='CC[N+]([O-])(CC)CCCN1c2ccccc2S(=O)c2ccccc21')
+        self.assertEqual(objs.count(), 4)
 
         
         
