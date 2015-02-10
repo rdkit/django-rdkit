d5cd030ad21da9fb8fb54845b47f3ab0c1ab87fe
Riccardo Vianello <riccardo.vianello@gmail.com>
2015-02-11 00:07:33 +0100

    tanimoto and dice distance operators now available as expression objects.

--- django_rdkit/db/models/functions.py
+++ django_rdkit/db/models/functions.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import sys
 
 from django.db import models
+from django.db.models.expressions import Expression
 
 from django_rdkit.db.models.descriptors import DESCRIPTOR_MIXINS
 from django_rdkit.db.models.fields import *
@@ -12,9 +13,9 @@ __all__ = []
 module = sys.modules[__name__]
 
 for mixin in DESCRIPTOR_MIXINS:
-    F = type(str(mixin.descriptor_name.upper()), (mixin, models.Func,), {})
-    setattr(module, F.__name__, F)
-    __all__.append(F.__name__)
+    _F = type(str(mixin.descriptor_name.upper()), (mixin, models.Func,), {})
+    setattr(module, _F.__name__, _F)
+    __all__.append(_F.__name__)
 
 
 for function, fieldkls in [('mol', MolField),
@@ -29,17 +30,17 @@ for function, fieldkls in [('mol', MolField),
                            ('mol_inchikey', models.TextField),
                            ('mol_formula', models.TextField),
                        ]:
-    F = type(str(function.upper()), (models.Func,), 
+    _F = type(str(function.upper()), (models.Func,), 
              { 'function': function, 'output_field': fieldkls(),})
-    setattr(module, F.__name__, F)
-    __all__.append(F.__name__)
+    setattr(module, _F.__name__, _F)
+    __all__.append(_F.__name__)
 
 
 for validator in ['is_valid_smiles', 'is_valid_smarts', 'is_valid_ctab']:
-    F = type(str(validator.upper()), (models.Func,), 
+    _F = type(str(validator.upper()), (models.Func,), 
              { 'function': validator, 'output_field': models.BooleanField(),})
-    setattr(module, F.__name__, F)
-    __all__.append(F.__name__)
+    setattr(module, _F.__name__, _F)
+    __all__.append(_F.__name__)
 
 
 for fingerprint, fieldkls in [('morgan_fp', SfpField),
@@ -56,12 +57,38 @@ for fingerprint, fieldkls in [('morgan_fp', SfpField),
                               ('tanimoto_sml', models.FloatField),
                               ('dice_sml', models.FloatField),
                               ('tversky_sml', models.FloatField),
-                              ('tanimoto_dist', models.FloatField),
-                              ('dice_dist', models.FloatField),
+                              #('tanimoto_dist', models.FloatField),
+                              #('dice_dist', models.FloatField),
                           ]:
-    F = type(str(fingerprint.upper()), (models.Func,), 
+    _F = type(str(fingerprint.upper()), (models.Func,), 
              { 'function': fingerprint, 'output_field': fieldkls(),})
-    setattr(module, F.__name__, F)
-    __all__.append(F.__name__)
+    setattr(module, _F.__name__, _F)
+    __all__.append(_F.__name__)
 
 
+class TANIMOTO_DIST(Expression):
+
+    def __init__(self, lhs, rhs):
+        lhs, rhs = [
+            arg if hasattr(arg, 'resolve_expression') else models.F(arg)
+            for arg in (lhs, rhs)
+        ]
+        super(TANIMOTO_DIST, self).__init__(lhs, '<%%>', rhs, 
+                                            models.FloatField())
+
+
+__all__.append('TANIMOTO_DIST')
+
+
+class DICE_DIST(Expression):
+
+    def __init__(self, lhs, rhs):
+        lhs, rhs = [
+            arg if hasattr(arg, 'resolve_expression') else models.F(arg)
+            for arg in (lhs, rhs)
+        ]
+        super(DICE_DIST, self).__init__(lhs, '<#>', rhs, 
+                                            models.FloatField())
+
+
+__all__.append('DICE_DIST')
