5f7c094f3f119562ba9aed8f01d3b9d4bd5c7dc8
Riccardo Vianello <riccardo.vianello@gmail.com>
2015-03-05 21:50:18 +0100

    run tests using the django-rdkit backend

---
+++ runtests-backend.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os, sys
+
+import django
+from django.conf import settings
+from django.core.management import call_command
+
+DEFAULT_SETTINGS = dict(
+    INSTALLED_APPS=(
+        #'django_rdkit',
+        'tests',
+    ),
+    DATABASES={
+        "default": {
+            #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django_rdkit.backend',
+            'NAME': 'test',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        }
+    },
+)
+
+
+def runtests():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+
+    django.setup()
+
+    parent = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, parent)
+
+    # if the postgresql_psycopg2 backend is used and the 'tests' application
+    # is not migrated, it will be synced before the rdkit extension is
+    # installed and therefore causing errors.
+    #call_command('makemigrations', 'tests')
+
+    #call_command('sqlmigrate', 'tests', '0001')
+
+    sys.exit(call_command('test', 'tests')) #.tests.RxnFieldTest.test_descriptors'))
+
+if __name__ == '__main__':
+    runtests()
