30cac1f61a1d401a5020a5e410a648fd4b2bfbda
Riccardo Vianello <riccardo.vianello@gmail.com>
2016-03-12 16:21:10 +0100

    updated version label

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_packages(package):
 
 setup(
     name='django-rdkit',
-    version='0.0.4',
+    version='0.0.5',
     description='',
     packages = get_packages('django_rdkit'),
     zip_safe=False,
