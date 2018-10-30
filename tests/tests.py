29248803f746606dfd59ad27625a411291bcb441
Riccardo Vianello <riccardo.vianello@gmail.com>
2018-10-30 22:22:55 +0100

    add a few tests using sql expressions to assign or filter a MolField column

--- tests/tests.py
+++ tests/tests.py
@@ -21,6 +21,11 @@ class MolFieldTest(TestCase):
         objs = MoleculeModel.objects.filter(molecule=None)
         self.assertEqual(objs.count(), 1)
 
+    def test_assign_from_sql_expression(self):
+        mol = MoleculeModel.objects.create(molecule=MOL_FROM_SMILES(Value('c1ccccc1')))
+        updated = MoleculeModel.objects.filter(id=mol.id).update(molecule=MOL_FROM_SMILES(Value('c1ccccc1')))
+        self.assertEqual(updated, 1)
+
     def test_exact_lookup(self):
 
         objs = MoleculeModel.objects.filter(molecule='COC(c1ccccc1)c1ccccc1')
@@ -28,13 +33,20 @@ class MolFieldTest(TestCase):
 
         objs = MoleculeModel.objects.filter(molecule='Nc1ccc(Cl)nc1')
         self.assertEqual(objs.count(), 1)
-        
+
         objs = MoleculeModel.objects.filter(molecule=Chem.MolFromSmiles('Nc1ccc(Cl)nc1'))
+        self.assertEqual(objs.count(), 1)
+
+        objs = MoleculeModel.objects.filter(molecule=MOL_FROM_SMILES(Value('Nc1ccc(Cl)nc1')))
         self.assertEqual(objs.count(), 1)
 
     def test_hassubstruct_lookup(self):
 
         objs = MoleculeModel.objects.filter(molecule__hassubstruct='C1=C(C)C=CC=C1')
+        self.assertEqual(objs.count(), 61)
+
+        objs = MoleculeModel.objects.filter(
+            molecule__hassubstruct=MOL_FROM_SMILES(Value('C1=C(C)C=CC=C1')))
         self.assertEqual(objs.count(), 61)
 
         objs = MoleculeModel.objects.filter(molecule__hassubstruct='C1=CC=CC=C1')
@@ -56,7 +68,7 @@ class MolFieldTest(TestCase):
         qmol = QMOL(Value('c1[c,n]cccc1'))
         objs = MoleculeModel.objects.filter(molecule__hassubstruct=qmol)
         self.assertEqual(objs.count(), cnt3)
-        
+
         objs = MoleculeModel.objects.filter(molecule__hassubstruct=Chem.MolFromSmiles('C1=CN=CC=C1'))
         cnt4 = objs.count()
         self.assertEqual(cnt2, 7)
@@ -68,7 +80,7 @@ class MolFieldTest(TestCase):
 
         objs = MoleculeModel.objects.filter(molecule__issubstruct='CC[N+]([O-])(CC)CCCN1c2ccccc2S(=O)c2ccccc21')
         self.assertEqual(objs.count(), 4)
-        
+
         objs = MoleculeModel.objects.filter(molecule__issubstruct=Chem.MolFromSmiles('CC[N+]([O-])(CC)CCCN1c2ccccc2S(=O)c2ccccc21'))
         self.assertEqual(objs.count(), 4)
 
