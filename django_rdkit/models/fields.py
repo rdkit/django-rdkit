bba529574d68d3f679252fbe7da25a002557a3ee
Ádám Baróthi <68119654+adam-of-barot@users.noreply.github.com>
2023-04-19 14:21:03 +0200

    Restore exact structure lookup
    Restore exact structure lookup to use exact match operator instead of 2 substruct. operators

--- django_rdkit/models/fields.py
+++ django_rdkit/models/fields.py
@@ -335,8 +335,8 @@ class SameStructure(MolLookupMixin, Lookup):
         lhs, lhs_params = self.process_lhs(qn, connection)
         rhs, rhs_params = self.process_rhs(qn, connection)
         params = lhs_params + rhs_params
-        #return '%s @= %s' % (lhs, rhs), params
-        return '%s <@ %s AND %s @> %s' % (lhs, rhs, lhs, rhs), params + params
+        return '%s @= %s' % (lhs, rhs), params
+        #return '%s <@ %s AND %s @> %s' % (lhs, rhs, lhs, rhs), params + params
 
 MolField.register_lookup(SameStructure)
 
